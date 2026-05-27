6b7511ccc5edc7b8ba7f4c90650203f2f8aac096
Naveen-ce <naveenmurugesan1972@gmail.com>
2026-05-27 21:06:28 +0530

    Commiting files

--- Full adder/Testbench/Environment.sv
+++ Full adder/Testbench/Environment.sv
@@ -49,7 +49,26 @@ class environment;
           m.mon();
           r.rf();
           s.sc();
-        join
-      endtask
+        join_any
+        #0;
+        
+        disable fork;
+          
+          pr=(s.pass/s.total)*100;
+          
+          $display("+------------------------------------------------+");
+          $display("|        TOTAL NUMBER OF TEST CASES =%2d          |",s.total);
+          $display("|------------------------------------------------|");
+          $display("|          PASS           |          FAIL        |");
+          $display("|                         |                      |");
+          $display("|          %3d            |          %3d         |",s.pass,s.fail);
+          $display("|                         |                      |");
+          $display("|------------------------------------------------|");
+          $display("              SUCCESS RATE = %0f                   ",pr);
+          $display("|------------------------------------------------|");
+          
+          #1; 
+          
+          endtask
   
 endclass:environment
